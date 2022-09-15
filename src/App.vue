6eff5580eb3e030c33f9acca3ffc4f6469b38c3a
Steven Lei <stevenplus@gmail.com>
2022-09-16 00:46:15 +0800

    setup firebase

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
-<script setup></script>
+<script setup>
+import { onMounted } from "vue";
+
+import { db } from "../firebaseInit";
+import { query, collection, getDocs } from "firebase/firestore";
+</script>
 
 <template>
   <h1 class="text-center text-3xl font-bold my-4 text-blue-800">Hello World!</h1>
